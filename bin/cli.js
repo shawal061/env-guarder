3a7f5c96f008ca44d1279164e8d2dbc01f88c8af
shawal061 <contact.shakhawat.hossain@gmail.com>
2026-04-05 02:38:36 +0600

    feat: add chalk for colored CLI output and update package metadata

--- bin/cli.js
+++ bin/cli.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 import dotenv from "dotenv";
+import chalk from "chalk";
 
 dotenv.config();
 
 const requiredVars = process.argv.slice(2);
 
 if (requiredVars.length === 0) {
-    console.log("⚠️  Please provide env variables to check:");
-    console.log("Example: env-guarder PORT DB_URL");
+    console.log(chalk.yellow("⚠️  Please provide env variables to check"));
+    console.log(chalk.gray("Example: env-guarder PORT DB_URL"));
     process.exit(0);
 }
 
 const missing = requiredVars.filter(v => !process.env[v]);
 
 if (missing.length > 0) {
-    console.log("❌ Missing environment variables:");
-    missing.forEach(v => console.log(` - ${v}`));
+    console.log(chalk.red("❌ Missing environment variables:"));
+    missing.forEach(v => console.log(chalk.red(` - ${v}`)));
     process.exit(1);
 } else {
-    console.log("✅ All required env variables are set!");
+    console.log(chalk.green("✅ All required env variables are set!"));
 }
